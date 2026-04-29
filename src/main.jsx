e580d5afcf3d4b3118763b7a27bd81fe9b60a6ea
DrdmRandom <dawwi.rdm@gmail.com>
2026-04-29 13:49:41 +0700

    Fixing stuff

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { init } from "@plausible-analytics/tracker";
 import App from "./App";
 import "./styles.css";
+
+init({
+  domain: "fachrulyporto.cihuy-familly.my.id",
+  endpoint: "https://analytics.cihuyproject.my.id/api/event",
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
